7d69f261b6187d3daed5012d77fe6bc523b2d802
Rookieville <rdanso2003@gmail.com>
2024-11-04 19:03:03 +0000

    created library class methods

--- LibraryManagementSystem/Library.cs
+++ LibraryManagementSystem/Library.cs
@@ -2,10 +2,86 @@ using System;
 using System.Collections.Generic;
 
 
+
 namespace LibraryManagementSystem
 {
     public class Library
     {
         private List<Book> books = new List<Book>();
+
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine("Book added successfully");
+        }
+
+
+        public void ViewBooks()
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books available");
+            }
+            else
+            {
+                Console.WriteLine("Books in the library:");
+                foreach (var book in books)
+                {
+
+                    Console.WriteLine(book);
+                }
+            }
+        }
+
+        //helper method to find books
+        public Book FindBookById(int id)
+        {
+            foreach (var book in books)
+            {
+                if (book.ID == id)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+
+
+        public void BorrowBook(int id)
+        {
+            Book book = FindBookById(id);
+            if (book == null)
+            {
+                Console.WriteLine("Book not found");
+            }
+            else if (book.IsBorrowed)
+            {
+                Console.WriteLine("Book is already borrowed");
+            }
+            else
+            {
+                book.IsBorrowed = true;
+                Console.WriteLine("Book borrowed successfully");
+            }
+        }
+
+        public void ReturnBook(int id)
+        {
+            Book book = FindBookById(id);
+            if (book == null)
+            {
+                Console.WriteLine("Book not found");
+            }
+            else if (!book.IsBorrowed)
+            {
+                Console.WriteLine("Book is not borrowed");
+            }
+            else
+            {
+                book.IsBorrowed = false;
+                Console.WriteLine("Book returned successfully");
+            }
+        }
     }
 }
