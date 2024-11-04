9002383cee27b5beeb9fe4315e67616feec4029e
Rookieville <rdanso2003@gmail.com>
2024-11-04 18:37:26 +0000

    initialized library class

---
+++ LibraryManagementSystem/Library.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace LibraryManagementSystem
+{
+    public class Library
+    {
+        private List<Book> books = new List<Book>();
+    }
+}
